97b5d5706954c3cc943d26c3007a06efeea0faf1
apnicollege <info.apnicollege@gmail.com>
2021-02-04 03:31:47 +0530

    operators-in-c

--- operators_precedence.c
+++ operators_precedence.c
@@ -6,6 +6,19 @@
  */
 #include <stdio.h>
 int main(){
+	int n=5;
+	n++; //n = n+1
+	n--;//n = n-1
+	printf("n=%d\n",n);
+
+	printf("*** increment inside print statement ***\n");
+	int x=10;
+	printf("x=%d",x++); //x++ = x=x+1
+	printf("x=%d\n",x);
+
+	int y =10;
+	printf("y=%d",y--);
+	printf("y=%d",y);
 	return 0;
 }
 
