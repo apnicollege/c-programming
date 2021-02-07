b33692545b30f59d2a36dde1bd1e2198c4f71480
apnicollege <info.apnicollege@gmail.com>
2021-02-07 15:22:55 +0530

    v14 - full star pattern

--- nested-for-full-star.c
+++ nested-for-full-star.c
@@ -6,17 +6,17 @@
  */
 #include <stdio.h>
 int main(){
-	for(int i=0;i<5;i++){ //i=0,1,2,3,4
-		/* print the spaces */
-		for(int s=5; s>i ;s--){ //5, 4, 3, 2, 1 = 5 spaces
+	for(int i=0; i<5 ;i++){ //i=0, 1, 2, 3, 4 = rows
+		/* print the spaces*/
+		for(int s=5;s>i ;s--){ //5>0(5s), 5>1(4s), 5>2(3s), 5>3(2s), 5>4(1s)
 			printf(" ");
 		}
-		/* print the stars on the right */
-		for(int j=0; j<=i ;j++){ //0(*), 1(**)...
+		/* print the left side stars*/
+		for(int j=0; j<=i ;j++){ //0<=0(1*), 0<=1(2*), 0<=2(3*),  0<=3(4*), 0<=4(5*)
 			printf("*");
 		}
-		/* print the stars on the left */
-		for(int k=0; k<i ;k++){
+		/* print the right side stars*/
+		for(int k=0; k<i;k++ ){ //k=0<0(), 0<1(1*), 0<2(2*), 0<3(3*), 0<4(4*)
 			printf("*");
 		}
 		printf("\n");
